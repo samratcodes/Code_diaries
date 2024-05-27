f1570b7db2661ae6f3d970a65204a317891d229b
SamratCodess <samratcodess@gmail.com>
2024-05-27 08:53:18 +0545

    redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-
+import { useState ,useEffect } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import{login,logout} from './store/authSlice'
 
 function App() {
+const [loading, setLoading] = useState(true)
+const dispatch = useDispatch()
+useEffect(() => {
+authService.getCurrentUser()
+.then((userData)=>{
+  if(userData){
+    dispatch(login({userData}))
+  }
+  else{
+    dispatch(logout())
+  }
+})
+.finally(()=>{
+ setLoading(false)
+})
 
-console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <h1>A blog app in appwrite</h1>
-    </>
-  )
+}, [])
+  return !loading ?
+  <div>
+  test
+  </div> :null
 }
 
 export default App
